87b961c1ea9c12af8af6be8e2da5eb76c3733ecf
rupam <rexrupam70@gmail.com>
2025-04-12 12:29:32 +0530

    Initial commmit

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 import express from "express"
 import HealthCheckRouter from "./router/blog.router.js"
 import bodyParser from "body-parser"
+import cors from "cors"
+
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(cors({
+    origin: process.env.cors_origin,
+    credentials: true
+}))
 
 app.use('/',HealthCheckRouter)
 export { app };
